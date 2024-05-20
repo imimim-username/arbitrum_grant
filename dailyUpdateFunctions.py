940ca69e2fade34f3d1ffc2d94ded45f39f8685c
AphoticJezter <jezter82@gmail.com>
2024-05-19 21:24:54 -0600

    Add getHarvestRepaid to gather harvest events, calculate crude APR for both donations and harvests for each harvest event

--- dailyUpdateFunctions.py
+++ dailyUpdateFunctions.py
@@ -1,20 +1,42 @@
-def getAddressDebt (blockNumber, alchemist, address):
+
+def rpcRequest(requestPayload):
+    from alchemyEndpoint import endpoint  #(inputQueryString, graphURL)
     import requests
     import json
     import time
+
+    apiCall = endpoint('arbitrum')
+
+    #print('API string: ', apiCall)
+
+    request_headers = {
+        "Content-Type": "application/json"
+    }
+    # alchemy api call request headers
+    while True:
+        api_post = requests.post(apiCall, headers=request_headers, data=json.dumps(requestPayload))
+        # calls the alchemy api to look up the token balance at the given block
+
+        result = api_post.json()
+        # turns the result into a usable json
+
+        if 'result' in result:
+            break #success getting result, exit loop to return
+
+        if 'error' in result:
+            print("Error calling RPC",result['error'])
+
+        time.sleep(0.5)
+        print("Retrying RPC request")
+
+    #print(result)
+    time.sleep(0.25)    #delay to avoid overwhelming RPC on success
+
+    return (result)
+
+
+def getAddressDebt (blockNumber, alchemist, address):
     from twos_complement import twos_complement
-
-    alchemyLocation = "/home/imimim/alchemix/user_debt/alchemy_api_key_arbitrum.txt"
-    api_key = open(alchemyLocation, "r")
-    # get the alchemy api key for network
-
-    keyValue = api_key.read()
-    # read the key from the file
-
-    api_key.close()
-    # close the opened file
-
-    apiCall = "https://arb-mainnet.g.alchemy.com/v2/" + keyValue
 
     dataStr = '0x5e5c06e2000000000000000000000000' + address[2:]
     #payload data string
@@ -32,16 +54,7 @@ def getAddressDebt (blockNumber, alchemist, address):
         ]
     } # payload to get address debt
 
-    request_headers = {
-        "Content-Type": "application/json"
-    }
-    # alchemy api call request headers'''
-
-    api_post = requests.post(apiCall, headers=request_headers, data=json.dumps(requestPayload))
-        # calls the alchemy mainnet API for alusd debt using the above settings
-
-    result = api_post.json()
-    # turns the result into a usable json
+    result = rpcRequest(requestPayload)
 
     result = twos_complement(result['result'][:66])
 
@@ -50,22 +63,7 @@ def getAddressDebt (blockNumber, alchemist, address):
     return(result)
 
 def convertToUnderlying (blockNumber, alchemist, yieldToken, numShares):
-    import requests
-    import json
-    import time
     from twos_complement import twos_complement
-
-    alchemyLocation = "/home/imimim/alchemix/user_debt/alchemy_api_key_arbitrum.txt"
-    api_key = open(alchemyLocation, "r")
-    # get the alchemy api key for network
-
-    keyValue = api_key.read()
-    # read the key from the file
-
-    api_key.close()
-    # close the opened file
-
-    apiCall = "https://arb-mainnet.g.alchemy.com/v2/" + keyValue
 
     dataStr = '0xa4a5da43000000000000000000000000' + str(yieldToken)[2:] + str(numShares)[2:]
 
@@ -85,44 +83,16 @@ def convertToUnderlying (blockNumber, alchemist, yieldToken, numShares):
 
     #print(requestPayload)
 
-    request_headers = {
-        "Content-Type": "application/json"
-    }
-    # alchemy api call request headers'''
-
-    api_post = requests.post(apiCall, headers=request_headers, data=json.dumps(requestPayload))
-        # calls the alchemy mainnet API for alusd debt using the above settings
-
-    result = api_post.json()
-    # turns the result into a usable json
-
-    #result = twos_complement(result['result'])
+    result = rpcRequest(requestPayload)
 
     print('Underlying balance')
     print(result)
 
-    time.sleep(0.25)
-    #pausing so as to not upset alchemy.
     return(twos_complement(result['result']))
 
 
 def getUserBalance (blockNumber, address, alchemist, yieldToken):
-    import requests
-    import json
-    import time
     from twos_complement import twos_complement
-
-    alchemyLocation = "/home/imimim/alchemix/user_debt/alchemy_api_key_arbitrum.txt"
-    api_key = open(alchemyLocation, "r")
-    # get the alchemy api key for network
-
-    keyValue = api_key.read()
-    # read the key from the file
-
-    api_key.close()
-    # close the opened file
-
-    apiCall = "https://arb-mainnet.g.alchemy.com/v2/" + keyValue
 
     dataStr = '0x4bd21445000000000000000000000000' + address[2:]
 
@@ -142,15 +112,7 @@ def getUserBalance (blockNumber, address, alchemist, yieldToken):
     }
     # payload to make api calls
 
-    request_headers = {
-        "Content-Type": "application/json"
-    }
-    # alchemy api call request headers
-
-    api_post = requests.post(apiCall, headers=request_headers, data=json.dumps(requestPayload))
-        # calls the alchemy mainnet API for alusd debt using the above settings
-
-    result = api_post.json()
+    result = rpcRequest(requestPayload)
     # turns the result into a usable json
 
     result = result['result'][0:66]
@@ -159,20 +121,10 @@ def getUserBalance (blockNumber, address, alchemist, yieldToken):
     print('Number of shares')
     print(result)
 
-    time.sleep(0.25)
-    #pausing so as to not upset alchemy
     return(result)
 
 
 def getTokenBalance (blockNumber, tokenAddress, address):
-
-    from alchemyEndpoint import endpoint  #(inputQueryString, graphURL)
-    import requests
-    import json
-
-    apiCall = endpoint('arbitrum')
-
-    #print('API string: ', apiCall)
 
     dataStr = '0x70a08231000000000000000000000000' + address[2:]
 
@@ -189,28 +141,15 @@ def getTokenBalance (blockNumber, tokenAddress, address):
         ]
     }
 
-    request_headers = {
-        "Content-Type": "application/json"
-    }
-    # alchemy api call request headers
+    result = rpcRequest(requestPayload)
 
-    api_post = requests.post(apiCall, headers=request_headers, data=json.dumps(requestPayload))
-    # calls the alchemy api to look up the token balance at the given block
-
-    result = api_post.json()
-    # turns the result into a usable json
+    print(result)
 
     return (result['result'])
 
 
 def convertYieldTokenBalanceToUnderlying (blockNumber, alchemist, yieldToken, balance):
-
-    from alchemyEndpoint import endpoint  #(inputQueryString, graphURL)
-    import requests
-    import json
     from twosComplement import twos_complement
-
-    apiCall = endpoint('arbitrum')
 
     dataStr = '0x46bb87c2000000000000000000000000' + yieldToken[2:] + balance[2:]
 
@@ -227,17 +166,8 @@ def convertYieldTokenBalanceToUnderlying (blockNumber, alchemist, yieldToken, ba
         ]
     }
 
-    request_headers = {
-        "Content-Type": "application/json"
-    }
-    # alchemy api call request headers
-
     print('looking up underlying')
-    api_post = requests.post(apiCall, headers=request_headers, data=json.dumps(requestPayload))
-    # calls the alchemy api to look up the token balance at the given block
-
-    result = api_post.json()
-    # turns the result into a usable json
+    result = rpcRequest(requestPayload)
 
     print('Underlying')
     print(result['result'])
@@ -245,3 +175,45 @@ def convertYieldTokenBalanceToUnderlying (blockNumber, alchemist, yieldToken, ba
 
     return(twos_complement(result['result']))
 
+def getHarvestRepaid (blockNumber,alchemist,harvestTxId):
+    from twosComplement import twos_complement
+
+    requestPayload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "eth_getLogs",
+        "params": [
+            {
+                "address": alchemist,
+                "fromBlock": hex(blockNumber),
+                "toBlock": hex(blockNumber),
+                #"topics": ["0x88dcaca629d63d86330e97adc358b13dd0ebd703239aea96b7ea2fb331b16f4e"] #topic for Donate(address sender, address yieldToken, uint256 amount)
+            }
+        ]
+    }
+
+    result = rpcRequest(requestPayload)
+
+    print(result)
+    answer = {'donation': 0, 'yieldRepaid': 0}
+
+    for event in result['result']:
+        if event['transactionHash']==harvestTxId:
+            match event['topics'][0]:
+                case '0x4534f107610758c3931de9ad1e176476fcfb8c74adf920167e1d54ee84fcfe76':
+                    #harvest event
+                    #topic[1] = yieldToken
+                    #data[2:66] = minimumAmountOut
+                    #data[66:130] = totalHarvested
+                    #data[130:194] = credit
+                    answer['yieldRepaid'] += twos_complement("0x"+event['data'][130:194])
+                    print("harvest", answer['yieldRepaid'])
+                case '0x88dcaca629d63d86330e97adc358b13dd0ebd703239aea96b7ea2fb331b16f4e':
+                    #donate event
+                    #topic[1] = sender
+                    #topic[2] = yieldToken
+                    #data[2:66] = amount
+                    answer['donation'] += twos_complement(event['data'])
+                    print("donate",answer['donation'])
+
+    return (answer)
--- harvestSnapshots.py
+++ harvestSnapshots.py
@@ -5,6 +5,7 @@ from subgraphRequest import subgraphRequest  #(inputQueryString, graphURL)
 from alchemyEndpoint import endpoint #(network)
 from dailyUpdateFunctions import getTokenBalance # (blockNumber, tokenAddress, address) convertYieldTokenBalanceToUnderlying (blockNumber, alchemist, yieldToken, balance):
 from dailyUpdateFunctions import convertYieldTokenBalanceToUnderlying #(blockNumber, alchemist, yieldToken, balance)
+from dailyUpdateFunctions import getHarvestRepaid #(blockNumber, alchemist, harvestTxId)
 
 harvestQuery = '''
 {
@@ -35,15 +36,38 @@ graphURL = 'https://api.goldsky.com/api/public/project_cltwyhnfyl4z001x17t5odo5x
 
 harvestEvents = subgraphRequest(harvestQuery, graphURL)
 
+#assume each yieldToken uses a single alchemist
+yieldTokens = set()
+
 for event in harvestEvents:
 #flattening the response, and adding prior block number, and getting yield token balance and underlying balance for that prior block
 
+    yieldTokens.add(event['yieldToken'])
     event['block'] = int(event['block']['number'])
     event['contract'] = event['contract']['id']
     event['transaction'] = event['transaction']['hash']
     event['priorBlock'] = hex(event['block'] - 1)
+    harvestRepaid = getHarvestRepaid(event['block'],event['contract'],event['transaction'])
+    event['donation'] = harvestRepaid['donation']
+    event['repaid'] = harvestRepaid['yieldRepaid']
     event['balance'] = getTokenBalance(event['priorBlock'], event['yieldToken'], event['contract'])
     event['underlyingBalance'] = convertYieldTokenBalanceToUnderlying(event['priorBlock'], event['contract'], event['yieldToken'], event['balance'])
     #print(event)
+
+for yieldToken in yieldTokens:
+    previousHarvest = {}
+    for event in harvestEvents:
+        if event['yieldToken']!=yieldToken:
+            continue
+        #skip first harvest, as we need an interval for calculating yield
+        if bool(previousHarvest):
+            underlyingBalance = event['underlyingBalance']
+            if yieldToken=='0x248a431116c6f6fcd5fe1097d16d0597e24100f5': #we actually care more about the underlying token decimals, but this works
+                underlyingBalance = underlyingBalance * 1e12 #convert from 1e6 to 1e18 (USDC to alAsset)
+            duration = int(event['timestamp'])-int(previousHarvest['timestamp'])
+            harvestAPR = (event['repaid']/underlyingBalance)*(365*24*60*60*100)/duration
+            donationAPR = (event['donation']/underlyingBalance)*(365*24*60*60*100)/duration
+            print(event['timestamp'],yieldToken,harvestAPR,donationAPR)
+        previousHarvest = event
 
 print(harvestEvents)
